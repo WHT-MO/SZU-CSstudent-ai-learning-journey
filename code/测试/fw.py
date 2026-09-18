c4fe49cf0eeff16e0ab95289ef050af0a0603ac0
szu-student <WHT-MO@users.noreply.github.com>
2026-09-18 17:46:49 +0800

    删除草稿区

--- code/测试/fw.py
+++
@@ -1,6 +0,0 @@
-with open("demo.txt","w",encoding="utf-8") as f:
-    last,d = map(int,input().split())
-    f.write(f"上次打卡第{last}天\n连续打卡{d}天")
-
-with open("demo.txt","r",encoding="utf-8") as f:
-    print(f.read())
--- code/测试/ordchr.py
+++
@@ -1,4 +0,0 @@
-y = ""
-for nums in range(65,91):
-    y += chr(nums)
-print(y)
